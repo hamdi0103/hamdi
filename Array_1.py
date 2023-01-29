1c76d90070972a0a3792c904e0b8aa53bd3467a0
hamdi0103 <123875090+hamdi0103@users.noreply.github.com>
2023-01-29 20:06:55 +0700

    array 2

--- Array_1.py
+++ Array_1.py
@@ -1,7 +1,7 @@
-dari  array  impor  array
+dari impor array
 import  itertools
 
-arr  =  array ( 'i' , [ 10 , 20 , 30 , 40 , 50 ])
+arr = array ( 'i' , [ 10 , 20 , 30 , 40 , 50 ])
 
 cetak ( arr [ 0 ])
 cetak ( arr [ 2 ])
@@ -14,7 +14,7 @@ operasi2  =  arr [ 1 ] +  arr [ 1 ] *  arr [ 0 ]
 print ( "{} + {} x {} = {}" .format ( arr [ 0 ] , arr [ 1 ], arr [ 0 ], operation1 ))
 cetak ( "{} + {} x {} = {}" . format ( arr [ 1 ], arr [ 1 ], arr [ 0 ], operation2 ))
 
-cetak ()
+cetak()
 
 bil1  =  larik ( "i" , [ 4 , 5 , 6 ])
 bil2  =  larik ( "i" , [ 10 , 20 , 30 , 40 , 50 , 60 ])
